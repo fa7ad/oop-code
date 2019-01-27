c6dabb1a6bf8f358094d9abcf6b7ac042d50afb9
Fahad Hossain <evilinfinite7@gmail.com>
2019-01-27 07:11:55 +0600

    add main function for triangle code

--- Triangle/src/triangle/Main.java
+++ Triangle/src/triangle/Main.java
@@ -3,8 +3,11 @@ package triangle;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Triangle t1 = new Triangle(4, 4, 4);
+        t1.testTriangle();
+        
+        Triangle t2 = new Triangle(5, 4, 5);
+        t2.testTriangle();
     }
 
 }
